f9251bfd06af322a5742a8c16669fb6d804e3ca0
Andrew Varvel <andy@fastmail.com.au>
2014-08-24 08:14:35 +1000

    Initial Commit

---
+++ CarFactory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CarFactory
+//
+//  Created by ANDREW VARVEL on 24/08/2014.
+//  Copyright (c) 2014 Andrew Varvel. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
